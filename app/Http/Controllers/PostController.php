9098b43ce3a3282b687bb368056de56de4464b62
EC2 Default User <ec2-user@ip-172-31-6-92.ec2.internal>
2023-08-16 17:44:13 +0900

    blog投稿詳細表示

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -20,6 +20,23 @@ class PostController extends Controller
     public function index(Post $post)//インポートしたPostをインスタンス化して$postとして使用
 
     {
-        return $post -> get();//$postの中身を戻り値にする
+      // return $post -> get();$postの中身を戻り値にする
+       return view('posts.index')->with(['posts'=>$post->getByLimit()]);
+    }
+    
+    
+    /*
+    
+    特定のIDのpostを表示する
+    
+    @params Object Post //引数の$postはid=1のPostインスタンス
+    @return Reposnse post view
+    
+    */
+    public function show(Post $post)
+    {
+        return view('posts.show')->with(['post'=>$post]);
+        //'post'はbladeファイルで使う変数。中身は$post  id=1のPostインスタンス
+        
     }
 }

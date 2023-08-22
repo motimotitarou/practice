85ead2e6de35961e583879622be2803b3c6125de
EC2 Default User <ec2-user@ip-172-31-6-92.ec2.internal>
2023-08-22 19:04:25 +0900

    とりあえずコミット

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\PostRequest;
 use Illuminate\Http\Request;
 use App\Models\Post;
 /**************
@@ -20,6 +21,50 @@ class PostController extends Controller
     public function index(Post $post)//インポートしたPostをインスタンス化して$postとして使用
 
     {
-        return $post -> get();//$postの中身を戻り値にする
+      // return $post -> get();$postの中身を戻り値にする
+       return view('posts.index')->with(['posts'=>$post->getByLimit(5)]);
     }
+    
+    
+    /*
+    
+    特定のIDのpostを表示する
+    
+    @params Object Post //引数の$postはid=1のPostインスタンス
+    @return Reposnse post view
+    
+    */
+    public function show(Post $post)
+    {
+        return view('posts.show')->with(['post'=>$post]);
+        //'post'はbladeファイルで使う変数。中身は$post  id=1のPostインスタンス
+        
+    }
+    public function create()
+    {
+        return view('posts.create');
+    }
+    
+    
+    public function store(Post $post ,PostRequest $request )
+{
+    $input = $request['post'];
+    $post->fill($input)->save();
+    return redirect('/posts/' . $post->id);
+}
+
+public function edit(Post $post)
+{
+    return view('posts.edit')->with(['post'=>$post]);
+}
+
+public function update(PostRequest $request,Post $post){
+    
+    $input_post = $request['post'];
+    $post->fill($input_post)->save();
+    
+    return redirect('/posts/' . $post->id);
+}
+
+
 }
